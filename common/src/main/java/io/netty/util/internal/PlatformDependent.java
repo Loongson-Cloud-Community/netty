9d1d74e90c613af79b690bde2e2ab2a1d15d9ac3
qiangxuhui <qiangxuhui@loongson.cn>
2023-04-27 11:19:42 +0000

    add loong64 support

--- common/src/main/java/io/netty/util/internal/PlatformDependent.java
+++ common/src/main/java/io/netty/util/internal/PlatformDependent.java
@@ -1506,6 +1506,9 @@ public final class PlatformDependent {
         if ("s390x".equals(value)) {
             return "s390_64";
         }
+        if ("loongarch64".equals(value)) {
+            return "loongarch_64";
+        }
 
         return "unknown";
     }
